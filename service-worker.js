80bed1237066b516fe5dfd17806da523569c25bd
Mahesh Hegde <mhegde@souq.com>
2015-11-30 18:14:19 +0530

    POC By Mahesh Hegde

---
+++ service-worker.js
@@ -0,0 +1,81 @@
+// The service worker running in background to receive the incoming
+// push notifications and user clicks
+
+// A push has arrived ...
+self.addEventListener('push', function(event) {  
+  // Since there is no payload data with the first version  
+  // of push messages, we'll grab some data from  
+  // an API and use it to populate a notification  
+  event.waitUntil(  
+    fetch('data.html').then(function(response) {  
+	 console.log(response);
+      if (response.status !== 200) {  
+        // Either show a message to the user explaining the error  
+        // or enter a generic message and handle the
+        // onnotificationclick event to direct the user to a web page  
+        console.log('Looks like there was a problem. Status Code: ' + response.status);  
+        throw new Error();  
+      }
+	  
+	   //console.log(response);
+	   var data = response;
+	   //alert(data);
+	  
+
+      // Examine the text in the response  
+      return response.json().then(function(data) {  
+        if (data.error || !data.notification) {  
+          console.error('The API returned an error.', data.error);  
+          throw new Error();  
+        }  
+
+        var title = data.notification.title;  
+        var message = data.notification.message;  
+        var icon = data.notification.icon;  
+		//var icon = 'img/realtime-logo.jpg'; 
+        var notificationTag = data.notification.tag;
+
+        return self.registration.showNotification(title, {  
+          body: message,  
+          icon: icon,  
+          tag: notificationTag  
+        });  
+      });  
+    }).catch(function(err) {  
+      console.error('Unable to retrieve data', err);
+
+      var title = 'An error occurred';
+      var message = 'We were unable to get the information for this push message';  
+      var icon = 'http://cf1.souqcdn.com/public/style/img/en/souq-logo-v2.png';  
+      var notificationTag = 'notification-error';  
+      return self.registration.showNotification(title, {  
+          body: message,  
+          icon: icon,  
+          tag: notificationTag  
+        });  
+    })  
+  );  
+});
+
+
+// The user has clicked on the notification ...
+self.addEventListener('notificationclick', function(event) {
+  // Android doesn’t close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)      
+      var url = 'http://uae.souq.com/ae-en/nikon-d7100-dslr-camera-24-1-mp-18-140mm-kit-lens-3-2-inch-screen-full-hd-black-7348864/i/';    
+      return clients.openWindow(url);
+  }));
+});
