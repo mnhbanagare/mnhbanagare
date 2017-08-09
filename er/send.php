6e5b5eec5f4df99f2cf2daad2a82ea7c109a06fc
Mahesh Hegde <mahesh.hegde@erosnow.com>
2017-08-09 10:49:31 +0530

    added er testing changes

--- er/send.php
+++ er/send.php
@@ -1,5 +1,5 @@
 <?php
-define("GOOGLE_API_KEY", "AAAAYMY0274:APA91bGTiMYdUzdlxR_udH2F_-Bd8w1AGVMcn5xBypizcen9fh7W4oZEjFbOBBfvO4a7S7C4CRhbGD_SW0q6kKV00z3eSsqqiuhvQ-H_GtqfyUqVk4EoK1WNdBPlH_5U-LdyqqrrYp-J");
+define("GOOGLE_API_KEY", "AAAAYMY0274:APA91bHaCW8gduEkBC6OkdHm9uaq3wGolCr9vb_71ZBW9wxiOLxdylL253Ce4ebbMUqlIHB2VELiFTN00uz63At3FCntBPhfX5FY7OyQ1KlK5kbsUi9eT-lHg3QwWGWlVz8FaFdc465K");
 define("GOOGLE_GCM_URL", "https://fcm.googleapis.com/fcm/send");
 
 function send_gcm_notify($reg_id, $message) {
@@ -35,24 +35,31 @@ function send_gcm_notify($reg_id, $message) {
 
 $reg_id = "cuTB1QMdhbE:APA91bGIWRCQ66NeSsbIoZeagzCXJBJBd49LSJ-yXVc5jHO7N5b44TUUXpSp2NxpHcZoVAw_LzQNB4G9R-wRLYcNptUiMUrkeUI4gHFAmWD5gtmNCn9Hhfta229Bm9RbXQ3WTWyZ4EdF";
 
-
 //$reg_id = $_REQUEST['rcd'];
 $msg = "Google Cloud Messaging Testing";
 
 send_gcm_notify($reg_id, $msg);
 
-
-$name = $_REQUEST['name'];
-$regid = $_REQUEST['rcd'];
-
-$msg = "New user identified with following is the details \n".$regid."\n".$name.                                                                                        "\n".  var_export($_REQUEST,true);
-$subject = "New browser :".$_REQUEST['rcd'];
-
-// use wordwrap() if lines are longer than 70 characters
+// $name = $_REQUEST['name'];
+// $regid = $_REQUEST['rcd'];
+// $msg = "New user identified with following is the details \n".$regid."\n".$name."\n".  var_export($_REQUEST,true);
+// $subject = "New browser :".$_REQUEST['rcd'];
 //$msg = wordwrap($msg,70);
-
 // send email
 // mail("mnhbanagare@gmail.com",$subject,$msg);
+
+// https://static-s.aa-cdn.net/img/ios/551666302/8fa45b88650d42e23a1548c8011a5f70?v=1
+
+// curl -X POST -H "Authorization: key=AAAAYMY0274:APA91bGTiMYdUzdlxR_udH2F_-Bd8w1AGVMcn5xBypizcen9fh7W4oZEjFbOBBfvO4a7S7C4CRhbGD_SW0q6kKV00z3eSsqqiuhvQ-H_GtqfyUqVk4EoK1WNdBPlH_5U-LdyqqrrYp-J" -H "Content-Type: application/json" -d '{
+//   "notification": {
+//     "title": "Portugal vs. Denmark",
+//     "body": "5 to 1",
+//     "icon": "firebase-logo.png",
+//     "click_action": "http://localhost:8081"
+//   },
+//   "to": "cuTB1QMdhbE:APA91bGIWRCQ66NeSsbIoZeagzCXJBJBd49LSJ-yXVc5jHO7N5b44TUUXpSp2NxpHcZoVAw_LzQNB4G9R-wRLYcNptUiMUrkeUI4gHFAmWD5gtmNCn9Hhfta229Bm9RbXQ3WTWyZ4EdF"
+// }' "https://fcm.googleapis.com/fcm/send"
+
 
 ?>
 
